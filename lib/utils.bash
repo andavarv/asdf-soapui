e1121dc35ef06731a8f9d4fe6f1b89f348b746b9
Andavar Veeramalai <andavar.veeramalai@healthstream.com>
2023-02-24 16:55:53 -0600

    Altered code to work with soapui url

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,73 +2,129 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for soapui.
-GH_REPO="https://github.com/SmartBear/soapui/releases"
-TOOL_NAME="soapui"
-TOOL_TEST="soapui --version"
+# tool name
+readonly tool_name="SoapUI"
 
 fail() {
-  echo -e "asdf-$TOOL_NAME: $*"
+  echo -e "asdf-$tool_name: $*"
   exit 1
 }
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if soapui is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
-sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if soapui has other means of determining installable versions.
-  list_github_tags
+  
+  # Refered form here https://github.com/webofmars/asdf-velero/
+
+  soapui_versions=smartbear/soapui
+  releases_path="https://api.github.com/repos/${soapui_versions}/releases"
+
+  cmd="curl -s"
+  
+  cmd="$cmd $releases_path"
+
+  function sort_versions() {
+    sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' | \
+      LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+  }
+
+  # Fetch all tag names, and get only second column. Then remove all unnecesary characters.
+  versions=$(eval "$cmd" | grep -oE "tag_name\": *\".{1,15}\"," | sed 's/tag_name\": *\"//;s/\",//' | sort_versions)
+  # shellcheck disable=SC2086
+  echo $versions
 }
 
-download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
 
-  # TODO: Adapt the release URL convention for soapui
-  url="$GH_REPO/archive/v${version}.tar.gz"
+get_binary_path_in_archive(){
+  local archive_dir=$1
+  local version=$2
+  local platform=$3
 
-  echo "* Downloading $TOOL_NAME release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  echo "$archive_dir/$tool_name-${version}-${platform}"
+}
+
+get_platform() {
+  local -r kernel="$(uname -s)"
+  if [[ ${OSTYPE} == "msys" || ${kernel} == "CYGWIN"* || ${kernel} == "MINGW"* ]]; then
+    echo windows
+  else
+    uname | tr '[:upper:]' '[:lower:]'
+  fi
+}
+
+get_filename() {
+  local version="$1"
+  local platform="$2"
+  
+  case "${platform}" in
+    linux)
+      echo "${tool_name}-${version}-${platform}-bin.tar.gz"
+      ;;
+    windows)
+      echo "${tool_name}-${version}-${platform}-bin.zip"
+      ;;
+    mac)
+      echo "${tool_name}-${version}-${platform}-bin.zip"
+      ;;
+    *)
+      echo "${tool_name}-${version}-linux-bin.tar.gz"
+      ;;
+  esac
+}
+
+get_download_url() {
+  local version="$1"
+  local platform="$2"
+  local filename
+  filename="$(get_filename "$version" "$platform")"
+  echo "https://dl.eviware.com/soapuios/${version}/${filename}"
 }
 
 install_version() {
-  local install_type="$1"
-  local version="$2"
-  local install_path="${3%/bin}/bin"
+  local -r install_type="$1"
+  local -r version="$2"
+  local -r install_path="$3"
+  
+   # make a temporary download directory with a cleanup hook
+  TMP_DOWNLOAD_DIR="$(mktemp -d -t "asdf_${tool_name}")"
+  readonly TMP_DOWNLOAD_DIR
+  trap 'rm -rf "${TMP_DOWNLOAD_DIR?}"' EXIT
+
 
   if [ "$install_type" != "version" ]; then
-    fail "asdf-$TOOL_NAME supports release installs only"
+    fail "asdf-soapui supports release installs only"
   fi
 
-  (
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+  local -r bin_install_path="${install_path}/bin"
+  local -r platform=$(get_platform)
+  local -r download_url=$(get_download_url "$version" "$platform")
+  local -r download_path="${TMP_DOWNLOAD_DIR}/${version}"
 
-    # TODO: Assert soapui executable exists.
-    local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+  echo "Downloading [${tool_name} $version] from ${download_url} to ${download_path}"
+  curl -Lo "$download_path" "$download_url"
 
-    echo "$TOOL_NAME $version installation was successful!"
-  ) || (
-    rm -rf "$install_path"
-    fail "An error occurred while installing $TOOL_NAME $version."
-  )
+  echo "Cleaning ${tool_name} previous binaries"
+  rm -rf "${bin_install_path:?}/${tool_name}"
+
+  echo "Creating ${tool_name} bin directory"
+  mkdir -p "${bin_install_path}"
+
+  echo "Extracting archive"
+  if [[ $platform == "linux" ]]; then 
+    tar xpf "$download_path" -C "$bin_install_path"
+  else 
+    unzip -qq "${download_path}" -d "${bin_install_path}"
+  fi
+    
+  echo "Copying binary"
+  # echo "${TMP_DOWNLOAD_DIR}"
+  # echo "${download_path}"
+  # echo "${bin_install_path}"
+  pwd
+  # cp "${TMP_DOWNLOAD_DIR}" "${bin_install_path}"
+  chmod +x "${bin_install_path}"
 }
