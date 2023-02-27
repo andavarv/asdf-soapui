2db0be157dd269189649a7d98c016fd58aaa763e
Andavar Veeramalai <andavar.veeramalai@healthstream.com>
2023-02-27 15:50:19 -0600

    Added new function

--- lib/utils.bash
+++ lib/utils.bash
@@ -16,17 +16,15 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
-# get_download_url() {
-#   local version="$1"
-#   local platform="$2"
-#   local filename
-#   filename="$(get_filename "$version" "$platform")"
-#   url="https://dl.eviware.com/soapuios/${version}/${filename}"
-#   echo "URL ==> $url"
-#   echo "* Downloading $TOOL_NAME release $version..."
-#   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
-#   # echo "https://dl.eviware.com/soapuios/${version}/${filename}"
-# }
+get_download_url() {
+  local version="$1"
+  local platform="$2"
+  local filename
+  filename="$(get_filename "$version" "$platform")"
+  url="https://dl.eviware.com/soapuios/${version}/${filename}"
+  echo "URL ==> $url"
+  echo "https://dl.eviware.com/soapuios/${version}/${filename}"
+}
 
 download_release() {
   local version="$1"
@@ -105,6 +103,57 @@ get_filename() {
 
 
 install_version() {
+  local -r install_type="$1"
+  local -r version="$2"
+  local install_path="${3%/bin}/bin"
+  
+   # make a temporary download directory with a cleanup hook
+  TMP_DOWNLOAD_DIR="$(mktemp -d -t "asdf_${TOOL_NAME}_XXXXXX")"
+  readonly TMP_DOWNLOAD_DIR
+  trap 'rm -rf "${TMP_DOWNLOAD_DIR?}"' EXIT
+
+
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-soapui supports release installs only"
+  fi
+
+  # local -r bin_install_path="${install_path}/bin"
+  local -r platform=$(get_platform)
+  local -r download_url=$(get_download_url "$version" "$platform")
+  local -r download_path="${TMP_DOWNLOAD_DIR}/$TOOL_NAME-$version"
+
+  echo "Downloading [${TOOL_NAME} $version] from ${download_url} to ${download_path}"
+  curl -Lo "$download_path" "$download_url"
+  # tar -zxvf SoapUI-5.7.0-linux-bin.tar.gz -C /Users/andavar.veeramalai/repos/asdf-soapui/bin/temp
+  (
+    # echo "Cleaning ${TOOL_NAME} previous binaries"
+    # rm -rf "${install_path:?}/${TOOL_NAME}"
+
+    echo "Creating ${TOOL_NAME} bin directory"
+    mkdir -p "${install_path}"
+    chmod +x "$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$version"
+    echo "Extracting archive"
+    if [[ $platform == "linux" ]] || [[ $platform == "darwin" ]]|| [[ $platform == "macos" ]]; then 
+      tar -zxvf "$download_path" -C "$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$version"
+    else 
+      unzip -qq "${ASDF_DOWNLOAD_PATH}" -d "${install_path}"
+    fi
+      
+    echo "Copying binary"
+    # echo "${TMP_DOWNLOAD_DIR}"
+    # echo "${download_path}"
+    # echo "${bin_install_path}"
+    # cp "${TMP_DOWNLOAD_DIR}" "${bin_install_path}"
+    chmod +x "$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$version"
+    cp "$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$version" "${install_path}"
+    echo "$TOOL_NAME $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error occurred while installing $TOOL_NAME $version."
+  )
+}
+
+install_version2() {
   local -r install_type="$1"
   local -r version="$2"
   local install_path="${3%/bin}/bin"
