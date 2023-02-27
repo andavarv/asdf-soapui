7faabfe720c55c66d2403690d9b891fdea45d9e2
Andavar Veeramalai <andavar.veeramalai@healthstream.com>
2023-02-27 13:30:49 -0600

    Build error fix

--- lib/utils.bash
+++ lib/utils.bash
@@ -16,6 +16,28 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
+get_download_url() {
+  local version="$1"
+  local platform="$2"
+  local filename
+  filename="$(get_filename "$version" "$platform")"
+  url="https://dl.eviware.com/soapuios/${version}/${filename}"
+  echo "* Downloading $tool_name release $version..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  # echo "https://dl.eviware.com/soapuios/${version}/${filename}"
+}
+
+download_release() {
+  local version="$1"
+  local -r platform=$(get_platform)
+  local filename
+  filename="$(get_filename "$version" "$platform")"
+  url="https://dl.eviware.com/soapuios/${version}/${filename}"
+  echo "* Downloading $tool_name release $version..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+}
+
+
 list_all_versions() {
   
   # Refered form here https://github.com/webofmars/asdf-velero/
@@ -39,13 +61,13 @@ list_all_versions() {
 }
 
 
-get_binary_path_in_archive(){
-  local archive_dir=$1
-  local version=$2
-  local platform=$3
+# get_binary_path_in_archive(){
+#   local archive_dir=$1
+#   local version=$2
+#   local platform=$3
 
-  echo "$archive_dir/$tool_name-${version}-${platform}"
-}
+#   echo "$archive_dir/$tool_name-${version}-${platform}"
+# }
 
 get_platform() {
   local -r kernel="$(uname -s)"
@@ -76,55 +98,56 @@ get_filename() {
   esac
 }
 
-get_download_url() {
-  local version="$1"
-  local platform="$2"
-  local filename
-  filename="$(get_filename "$version" "$platform")"
-  echo "https://dl.eviware.com/soapuios/${version}/${filename}"
-}
+
 
 install_version() {
   local -r install_type="$1"
   local -r version="$2"
-  local -r install_path="$3"
+  local install_path="${3%/bin}/bin"
   
    # make a temporary download directory with a cleanup hook
-  TMP_DOWNLOAD_DIR="$(mktemp -d -t "asdf_${tool_name}")"
-  readonly TMP_DOWNLOAD_DIR
-  trap 'rm -rf "${TMP_DOWNLOAD_DIR?}"' EXIT
+  # TMP_DOWNLOAD_DIR="$(mktemp -d -t "asdf_${tool_name}_XXXXXX")"
+  # readonly TMP_DOWNLOAD_DIR
+  # trap 'rm -rf "${TMP_DOWNLOAD_DIR?}"' EXIT
 
 
   if [ "$install_type" != "version" ]; then
     fail "asdf-soapui supports release installs only"
   fi
 
-  local -r bin_install_path="${install_path}/bin"
-  local -r platform=$(get_platform)
-  local -r download_url=$(get_download_url "$version" "$platform")
-  local -r download_path="${TMP_DOWNLOAD_DIR}/${version}"
+  # local -r bin_install_path="${install_path}/bin"
+  # local -r platform=$(get_platform)
+  # local -r download_url=$(get_download_url "$version" "$platform")
+  # local -r download_path="${TMP_DOWNLOAD_DIR}/${version}"
 
-  echo "Downloading [${tool_name} $version] from ${download_url} to ${download_path}"
-  curl -Lo "$download_path" "$download_url"
+  # echo "Downloading [${tool_name} $version] from ${download_url} to ${download_path}"
+  # curl -Lo "$download_path" "$download_url"
+  # tar -zxvf SoapUI-5.7.0-linux-bin.tar.gz -C /Users/andavar.veeramalai/repos/asdf-soapui/bin/temp
+  (
+    echo "Cleaning ${tool_name} previous binaries"
+    rm -rf "${install_path:?}/${tool_name}"
 
-  echo "Cleaning ${tool_name} previous binaries"
-  rm -rf "${bin_install_path:?}/${tool_name}"
+    echo "Creating ${tool_name} bin directory"
+    mkdir -p "${install_path}"
 
-  echo "Creating ${tool_name} bin directory"
-  mkdir -p "${bin_install_path}"
-
-  echo "Extracting archive"
-  if [[ $platform == "linux" ]]; then 
-    tar xpf "$download_path" -C "$bin_install_path"
-  else 
-    unzip -qq "${download_path}" -d "${bin_install_path}"
-  fi
-    
-  echo "Copying binary"
-  # echo "${TMP_DOWNLOAD_DIR}"
-  # echo "${download_path}"
-  # echo "${bin_install_path}"
-  pwd
-  # cp "${TMP_DOWNLOAD_DIR}" "${bin_install_path}"
-  chmod +x "${bin_install_path}"
+    echo "Extracting archive"
+    if [[ $platform == "linux" ]] || [[ $platform == "darwin" ]]|| [[ $platform == "macos" ]]; then 
+      tar -zxvf "$ASDF_DOWNLOAD_PATH" -C "$ASDF_DOWNLOAD_PATH/$tool_name-$version"
+    else 
+      unzip -qq "${ASDF_DOWNLOAD_PATH}" -d "${install_path}"
+    fi
+      
+    echo "Copying binary"
+    # echo "${TMP_DOWNLOAD_DIR}"
+    # echo "${download_path}"
+    # echo "${bin_install_path}"
+    pwd
+    # cp "${TMP_DOWNLOAD_DIR}" "${bin_install_path}"
+    chmod +x "$ASDF_DOWNLOAD_PATH/$tool_name-$version"
+    cp "$ASDF_DOWNLOAD_PATH/$tool_name-$version" "${install_path}"
+    echo "$tool_name $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error occurred while installing $tool_name $version."
+  )
 }
