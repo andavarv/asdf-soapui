0e00c6d15158ec224dbb4e6c04b6dd4f1bf0ebdc
Andavar Veeramalai <andavar.veeramalai@healthstream.com>
2023-03-02 11:32:46 -0600

    added  tool cmd

--- lib/utils.bash
+++ lib/utils.bash
@@ -118,9 +118,10 @@ install_version() {
     chmod +x "$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$version"
     cp -Rv "$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$version" "${install_path}"
 
-    # local tool_cmd
+    local tool_cmd="testrunner.sh -help"
     # tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     # test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+    test -x "${install_path}/${tool_cmd}" || fail "Expected $install_path/$tool_cmd to be executable."
     
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
