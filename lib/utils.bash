e5de788e8979616e4da11e4434c1fa12e775f944
Andavar Veeramalai <andavar.veeramalai@healthstream.com>
2023-03-03 15:20:40 -0600

    move to plugins dir

--- lib/utils.bash
+++ lib/utils.bash
@@ -117,6 +117,9 @@ install_version() {
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
+    mkdir -p ${HOME}/.asdf/plugins/
+    mv "$ASDF_DOWNLOAD_PATH"/*  ${HOME}/.asdf/plugins/
+
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
     rm -rf "$install_path"
