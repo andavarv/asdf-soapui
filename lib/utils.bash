974792d7b443489dc2dae9707b3960d14025e2da
Andavar Veeramalai <andavar.veeramalai@healthstream.com>
2023-03-07 13:31:46 -0600

    soapui

--- lib/utils.bash
+++ lib/utils.bash
@@ -117,18 +117,6 @@ install_version() {
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
     echo "$TOOL_NAME $version installation was successful!"
-    pwd
-    ls -aR
-    if [ "$platform" != "linux" ]; then
-      echo "DOWNload path $ASDF_DOWNLOAD_PATH"
-      echo "INStall path $install_path"
-      echo "INStall HOME path ${HOME}"
-      pushd "${HOME}/.asdf/installs"
-      pwd
-    
-      popd
-    fi
-
 
   ) || (
     rm -rf "$install_path"
